b6f49a716cb5487d94d115b3b4344f4e204c0d95
Gaetano Foster <gaetano@gtantheta.gtantheta.com>
2026-02-26 00:46:20 -0500

    Updated how activation functions work to be more generalized

--- control-cnn/actfunc.h
+++ control-cnn/actfunc.h
@@ -1,6 +1,9 @@
 #ifndef ACTFUNC_H
 #define ACTFUNC_H
 
+#include <math.h>
+#include <stddef.h>
+
 enum actfunc_type {
 	ACTFUNC_RELU,
 	ACTFUNC_LEAKY,
@@ -10,32 +13,19 @@ enum actfunc_type {
 	ACTFUNC_SOFTMAX
 };
 
-double actfunc	(enum actfunc_type type, 
+// x = array of inputs
+// len = length of array
+// idx = index of array being operated on
+double 
+actfunc	(enum actfunc_type type, 
+	 double *x,
+	 size_t len,
+	 size_t idx);
+
+double 
+der_actfunc	(enum actfunc_type type, 
 		 double *x,
 		 size_t len,
 		 size_t idx);
-
-double der_actfunc	(enum actfunc_type type, 
-			 double *x,
-			 size_t len,
-			 size_t idx);
-
-double relu(double x);
-double leaky_relu(double x);
-double sigmoid(double x);
-double silu(double x);
-double softmax	(double *x,
-		 size_t len,
-		 size_t idx); // index must be in range of array x
-
-double der_relu(double x);
-double der_leaky_relu(double x);
-double der_sigmoid(double x);
-double der_tanh(double x);
-double der_silu(double x);
-double der_softmax	(double *x,
-			 size_t len,
-			 size_t idx);
-
 
 #endif // ACTFUNC_H
--- control-cnn/cost.h
+++ control-cnn/cost.h
@@ -6,31 +6,27 @@ enum cost_type {
 	COST_CROSS_ENTROPY
 };
 
-double cross_entropy	(double *predicted,
-			 double *expected,
-			 size_t len);
-
-double mean_square_err	(double *predicted,
-			 double *expected,
-			 size_t len);
-
-
-double der_cross_entropy	(double predicted,
-				 double expected);
-
-double der_mean_square_err	(double predicted,
-				 double expected);
-
-double costfunc	(enum cost_type type,
-		 double *predicted,
+double 
+cross_entropy	(double *predicted,
 		 double *expected,
 		 size_t len);
-
-
-double der_costfunc	(enum cost_type type,
-			 double predicted,
+double 
+mean_square_err	(double *predicted,
+		 double *expected,
+		 size_t len);
+double 
+der_cross_entropy	(double predicted,
 			 double expected);
-
-
-
+double 
+der_mean_square_err	(double predicted,
+			 double expected);
+double 
+costfunc(enum cost_type type,
+	 double *predicted,
+	 double *expected,
+	 size_t len);
+double 
+der_costfunc	(enum cost_type type,
+		 double predicted,
+		 double expected);
 #endif
